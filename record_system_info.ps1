2e6c88d83c44ef44a3e516e0f468555722e20f52
KIM, JINWOOK <zelonion@gmail.com>
2019-05-31 23:43:51 +0900

    Add some filters and clean up codes

--- record_system_info.ps1
+++ record_system_info.ps1
@@ -1,10 +1,14 @@
 chcp 65001
 mode 300
 Get-EventLog -LogName "System" -After ((Get-Date) + (New-TimeSpan -Days -7)) `
-  | Where-Object {$_.InstanceID -ne '10016' -and $_.InstanceID -ne '10010' } `
-  | Where-Object {$_.InstanceID -ne '1014' } <# there is no dns server #> `
-  | Where-Object {$_.InstanceID -ne '134' } <# NtpClient - time-service #> `
   | Where-Object {$_.EntryType -ne 'Information' } `
-  | Sort-Object Index
+  | Select-Object -Property TimeWritten, Source, EventID, Message `
+  | Where-Object {$_.EventID -ne '10016' } <# dcom error #> `
+  | Where-Object {$_.EventID -ne '10010' } <# dcom error2 #> `
+  | Where-Object {$_.EventID -ne '1014' } <# there is no dns server #> `
+  | Where-Object {$_.EventID -ne '134' } <# NtpClient - time-service #> `
+  | Where-Object {$_.EventID -ne '15301' } <# make new ssl cerificate #> `
+  | Where-Object {$_.EventID -ne '15300' } <# delete new ssl cerificate #> `
+  | Sort-Object TimeWritten
 Get-WmiObject -namespace root\wmi -class MSStorageDriver_FailurePredictStatus -ErrorAction Silentlycontinue | Select InstanceName, PredictFailure, Reason | Format-Table -Autosize
 Get-Date -Format g
